725f52638af1417ec2484d66e65e1c584dc6ecdc
ivamil <ivadent@yahoo.com>
2018-02-08 20:43:57 +0100

    i

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,43 @@
+package calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+
+			Scanner sc = new Scanner(System.in);  
+		     
+			System.out.println("Unesite prvi broj");  
+			int x = sc.nextInt(); 
+			
+			System.out.println("Izaberite operaciju +, -, *, / ");  
+			String op = sc.next(); 
+			
+			System.out.println("Unesite drugi broj");  
+			int y = sc.nextInt(); 
+			
+			int z = 0;
+			
+			if(op.equals("+"))
+			{
+				z = x + y;
+			}
+			else if(op.equals("-"))
+			{
+				z = x - y;
+			}
+			else if(op.equals("*"))
+			{
+				z = x * y;
+			}
+			else if(op.equals("/"))
+			{
+				z = x / y ;
+			}			
+			
+			System.out.println("Prvi broj je " + x);
+			System.out.println("Drugi broj je " + y);
+			System.out.println("Rezultat operacije je: " + z);
+
+	}
+	
+}
