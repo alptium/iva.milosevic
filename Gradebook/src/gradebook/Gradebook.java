1c7c76be4d02eea1604325c1e6cd75631e1931e3
ivamil <ivadent@yahoo.com>
2018-02-16 23:40:50 +0100

    zadatak

--- Gradebook/src/gradebook/Gradebook.java
+++ Gradebook/src/gradebook/Gradebook.java
@@ -6,26 +6,50 @@ public class Gradebook {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in); 
-			int x = sc.nextInt();
-		
 		System.out.println("STUDENT CLASS");
 		System.out.println("Teacher name: Mrs James");
 		
-		int[] list ={1, 2, 3, 4, 5};
+		String student1; 
+		String student2;
+		String student3;
+		String student4;
+		String student5;
+				
+				
+		int grade1;
+		int grade2;
+		int grade3;
+		int grade4;
+		int grade5;
+		int totalGrade;
 		
-		System.out.println("Student Number 1");
-		System.out.println("Student Number 2");
-		System.out.println("Student Number 3");
-		System.out.println("Student Number 4");
-		System.out.println("Student Number 5");
+		try(Scanner sc = new Scanner(System.in)) {
 		
-		String  = "Student 1";
-		String  = "Student 2";
-		String  = "Student 3";
-		String  = "Student 4";
-		String  = "Student 5";
+		System.out.println("Enter Grade, Students Name and Surname");
+		grade1 = sc.nextInt();
+		student1 = sc.next();
 		
+		System.out.println("Enter Grade, Students Name and Surname");
+		grade2 = sc.nextInt();
+		student2 = sc.next();
+		
+		System.out.println("Enter Grade, Students Name and Surname");
+		grade3 = sc.nextInt();
+		student3 = sc.next();
+		
+		System.out.println("Enter Grade, Students Name and Surname");
+		grade4 = sc.nextInt();
+		student4 = sc.next();
+		
+		System.out.println("Enter Grade, Students Name and Surname");
+		grade5 = sc.nextInt();
+		student5 = sc.next();
+		
+		totalGrade = (grade1 + grade2 + grade3 + grade4 + grade5) / 5 ;
+		System.out.println("Prosek je :" + totalGrade);
+		
+		}
+	
 	}
 
 }
