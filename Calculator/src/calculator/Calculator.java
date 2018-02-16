355566fbbddcc2efa0e383a4588e89279c6739ac
ivamil <ivadent@yahoo.com>
2018-02-17 00:00:57 +0100

    izmena

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
+		
+			int x;
+			int y;
+			int z;
 
 			try(Scanner sc = new Scanner(System.in)) {
 		     
 			System.out.println("Unesite prvi broj");  
-			int x = sc.nextInt(); 
+			x = sc.nextInt(); 
 			
 			System.out.println("Izaberite operaciju +, -, *, / ");  
 			String op = sc.next(); 
 			
 			System.out.println("Unesite drugi broj");  
-			int y = sc.nextInt(); 
+			y = sc.nextInt(); 
 			
-			int z = 0;
+	        z = 0;
 			
 			if(op.equals("+")){
 				z = x + y;
