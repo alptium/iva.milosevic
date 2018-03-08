804db1c7c90abd6dbe2a0003819146a313de9553
Valentina Cupac <valentina.cupac@optivem.com>
2018-03-08 20:53:44 +0100

    #4 Calculator - minor improvement

--- calculator/src/calculator/Main.java
+++ calculator/src/calculator/Main.java
@@ -6,22 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int x;
-		int y;
-		int z;
-		
-		z = 0;
-
 		try(Scanner sc = new Scanner(System.in)) {
 		     
 			System.out.println("Enter your first number");  
-			x = sc.nextInt(); 
+			int x = sc.nextInt(); 
 			
 			System.out.println("Choose the operation +, -, *, / ");  
 			String op = sc.next(); 
 			
 			System.out.println("Enter your second number");  
-			y = sc.nextInt(); 
+			int y = sc.nextInt(); 
+			
+			int z = 0;
 			
 			if(op.equals("+")) {
 				z = x + y;
