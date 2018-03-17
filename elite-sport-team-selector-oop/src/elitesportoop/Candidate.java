ddd37168da4b0309dc851965954345eab4524c93
Iva <34971209+ivamil@users.noreply.github.com>
2018-03-17 01:37:32 +0100

    changes

--- elite-sport-team-selector-oop/src/elitesportoop/Candidate.java
+++ elite-sport-team-selector-oop/src/elitesportoop/Candidate.java
@@ -9,14 +9,14 @@ public class Candidate {
 	private int height;
 	private int weight;
 	private String healthIssue;
-	private int acctiveYears;
+	private int activeYears;
 	private String position;
 	private double points;
 	private double assistance;
 	private String contract;
 	private String scholarship;
 	
-	public Candidate (long jmbg, String name, String surname, int birthYear, int height, int weight, String healthIssue, int acctiveYears, String position, double points, double assistance, String contract, String scholarship) {
+	public Candidate(long jmbg, String name, String surname, int birthYear, int height, int weight, String healthIssue, int activeYears, String position, double points, double assistance, String contract, String scholarship) {
 		this.jmbg =jmbg;
 		this.name = name;
 		this.surname = surname;
@@ -32,56 +32,56 @@ public class Candidate {
 		this.scholarship = scholarship;
 		
 	}
-		public long getjmbg() {
-			return jmbg;
-		}
-		
-		public String getname() {
-			return name;
-		}
-		
-		public String getsurname() {
-			return surname;
-		}
-		
-		public int getbirthYear() {
-			return birthYear;
-		}
-		
-		public int getheight() {
-			return height;
-		}
-		
-		public int getweight() {
-			return weight;
-		}
-		
-		public String gethealthIssue() {
-			return healthIssue;
-		}
-		
-		public int getacctiveYears() {
-			return acctiveYears;
-		}
-		public String getposition() {
-			return position;
-		}
-		
-		public double getPoints() {
-			return points;
-		}
-		
-		public double getAssistance() {
-			return assistance;
-		}
-		
-		public String getContract() {
-			return contract;
-		}
-		
-		public String getScholarship() {
-			return scholarship;
-		}
+	public long getJmbg() {
+		return jmbg;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public String getHealthIssue() {
+		return healthIssue;
+	}
+
+	public int getActiveYears() {
+		return activeYears;
+	}
+	public String getPosition() {
+		return position;
+	}
+
+	public double getPoints() {
+		return points;
+	}
+
+	public double getAssistance() {
+		return assistance;
+	}
+
+	public String getContract() {
+		return contract;
+	}
+
+	public String getScholarship() {
+		return scholarship;
+	}
+}
 
 
